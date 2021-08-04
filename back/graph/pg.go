b7678aed82977d9a7a1c3199c24cbb1ed02e1258
Alex Katebi <gkatebi@gmail.com>
2021-08-03 20:38:57 -0400

    f

--- back/graph/pg.go
+++ back/graph/pg.go
@@ -3,38 +3,32 @@ package graph
 import (
 	"database/sql"
 	"fmt"
+	"log"
+
 	_ "github.com/lib/pq"
 )
 
 const (
-	host     = "localhost"
+	host     = "postgres-db-lb"
 	port     = 5432
 	user     = "postgres"
-	password = "<password>"
-	dbname   = "<dbname>"
+	password = "testpassword"
+	dbname   = "/data/pgdata"
 )
 
 func init() {
-	// log.Printf("### Postgres Connect ###")
 	// connection string
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	// open database
 	db, err := sql.Open("postgres", psqlconn)
-	CheckError(err)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("### db ###, %v", db)
 
 	// close database
 	defer db.Close()
 
-	// check db
-	err = db.Ping()
-	CheckError(err)
-
 	fmt.Println("Connected!")
-}
-
-func CheckError(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
