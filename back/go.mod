ddad92204f1676ee86d8f219983f830be4cd11b6
Alex Katebi <gkatebi@gmail.com>
2021-08-15 02:33:54 -0400

    f

--- back/go.mod
+++ back/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/go-chi/chi v3.3.2+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/graphql-go/graphql v0.7.9 // indirect
 	github.com/graphql-go/relay v0.0.0-20171208134043-54350098cfe5
--- back/server.go
+++ back/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/akatebi/todos/graph/database"
 	"github.com/akatebi/todos/graph/generated"
+	"github.com/go-chi/chi"
 	"github.com/rs/cors"
 )
 
@@ -32,10 +33,23 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", CORS.Handler(srv))
+	router := chi.NewRouter()
+	router.Use(Middleware())
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", CORS.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
+}
+
+// Middleware decodes the share session cookie and packs the session into context
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// log.Printf("#### Request %#v", *r)
+			next.ServeHTTP(w, r)
+		})
+	}
 }
