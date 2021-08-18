8488048c92eb3c45aa5a5cce7a2c41f257212356
Alex Katebi <gkatebi@gmail.com>
2021-08-17 20:49:49 -0400

    f

--- test/test.js
+++ test/test.js
@@ -5,25 +5,29 @@ describe('Testing Todo GraphQL', () => {
     beforeAll(async() => {
         const email = "test@test.com";
         const resp = await User({email})
-        console.log("resp", JSON.stringify(resp, 0, 2));
+        // console.log("resp", JSON.stringify(resp, 0, 2));
+        expect(resp).toMatchSnapshot();
         userId = resp.data.user.id;
     });
     test("AddTodo", async() => {
       const clientMutationId = "1";
       const text = "Get a job";
       const resp = await AddTodo({text, userId, clientMutationId});
-      console.log("resp", JSON.stringify(resp, 0, 2));
+      // console.log("resp", JSON.stringify(resp, 0, 2));
+      expect(resp).toMatchSnapshot();
     });
     test("MarkAllTodos", async() => {
       const clientMutationId = "2";
       const complete = true;
       const resp = await MarkAllTodos({complete, userId, clientMutationId});
-      console.log("resp", JSON.stringify(resp, 0, 2));
+      // console.log("resp", JSON.stringify(resp, 0, 2));
+      expect(resp).toMatchSnapshot();
     });
     test("RemoveCompletedTodos", async() => {
       const clientMutationId = "3";
       const complete = true;
       const resp = await RemoveCompletedTodos({userId, clientMutationId});
-      console.log("resp", JSON.stringify(resp, 0, 2));
+      // console.log("resp", JSON.stringify(resp, 0, 2));
+      expect(resp).toMatchSnapshot();
     });
   });
