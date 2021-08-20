8619fd1090d99b1c473562155f6ecf4f913d9dae
Alex Katebi <gkatebi@gmail.com>
2021-08-20 04:41:25 -0400

    f

--- test/schema.js
+++ test/schema.js
@@ -10,6 +10,16 @@ const AddUser = ({email, clientMutationId}) => {
     return fetch({query, variables});
 }
 
+export const AddUserTest = (user) => () => {
+  it("", async() => {
+    const clientMutationId = user;
+    const email = `${user}@test.com`
+    const resp = await AddUser({ email, clientMutationId });
+    expect(resp).toMatchSnapshot();
+    global.userId = resp.data.addUser.id;
+  });
+}
+
 const RemoveUser = ({email, clientMutationId}) => {
   const variables = { email, clientMutationId };
   const query = `mutation RemoveUser($email: String!, $clientMutationId: String) {
@@ -19,11 +29,19 @@ const RemoveUser = ({email, clientMutationId}) => {
     return fetch({query, variables});
 }
 
+export const RemoveUserTest = (user) => () => {
+  it("", async() => {
+    const clientMutationId = user;
+    const email = `${user}@test.com`
+    const resp = await RemoveUser({ email, clientMutationId });
+    expect(resp).toMatchSnapshot();
+  });
+}
 
 const User = ({email}) => {
     const variables = { email }; 
-    const query = `query User($email: String!, $clientMutationId: String) {
-        user(email: $email, clientMutationId: $clientMutationId) {
+    const query = `query User($email: String!) {
+        user(email: $email) {
           id
           email
           completedCount
@@ -71,6 +89,19 @@ const query = `mutation addTodo($text: String!, $userId: ID!, $clientMutationId:
       }`;
       return fetch({query, variables});
     }
+
+  export const AddTodoTest = (txt) => () => {
+  it("", async () => {
+      global.todoIds = [];
+      for (let i = 0; i < 3; i++) {
+        const text = `${txt} ${i + 1}`;
+        const clientMutationId = `AddToDo-${i}`;
+        const resp = await AddTodo({ text, userId, clientMutationId });
+        expect(resp).toMatchSnapshot();
+        global.todoIds.push(resp.data.addTodo.todoEdge.node.id);
+      }
+    });
+  }  
 
 const MarkAllTodos = ({complete, userId, clientMutationId}) => {
   const variables = {complete, userId, clientMutationId};
@@ -138,16 +169,3 @@ const ChangeTodoStatus = ({complete, id, userId, clientMutationId}) => {
   `;
   return fetch({query, variables});
 }
-
-
-
-
-exports.AddUser = AddUser;
-exports.RemoveUser = RemoveUser;
-exports.User = User;
-exports.AddTodo = AddTodo; 
-exports.ClearCompletedTodos = ClearCompletedTodos; 
-exports.ChangeTodoStatus = ChangeTodoStatus; 
-exports.MarkAllTodos = MarkAllTodos; 
-// exports.RemoveTodo = RemoveTodo; 
-// exports.RenameTodo = RenameTodo; 
